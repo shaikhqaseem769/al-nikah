fa9874129944282d6346fa2413a29781f09dfefe
Mohammad Qaseem <mohammad.qaseem@aurumwisex.com>
2024-06-05 22:14:25 +0530

    sub community model

--- src/controllers/admin/subCommunity.controllers.js
+++ src/controllers/admin/subCommunity.controllers.js
@@ -1,7 +1,7 @@
 import { ApiResponse } from "../../utils/ApiResponse.js";
 import { ApiError } from "../../utils/ApiError.js";
 import { asyncHandler } from "../../utils/asyncHandler.js";
-import { SubCommunity } from "../../models/SubCommunity.models.js";
+import { SubCommunity } from "../../models/subCommunity.models.js";
 
 const addSubCommunity = asyncHandler(async (req, res) => {
   const { name, community } = req.body;
